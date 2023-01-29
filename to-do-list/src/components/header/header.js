fd6ff420c5a69dc5e65c9668d043b6f9ae00985e
Atakanz <atakanzer@gmail.com>
2023-01-29 05:08:55 +0300

    Enter keydown property added.

--- to-do-list/src/components/header/header.js
+++ to-do-list/src/components/header/header.js
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from 'react';
-import * as FaIcons from 'react-icons/fa';
+import React, {useState} from 'react';
 import './header.css';
 import {setList} from '../../management/features/listSlice';
 import {useDispatch} from 'react-redux';
@@ -7,20 +6,21 @@ import {useDispatch} from 'react-redux';
 const Header = () => {
   const [newTask, setNewTask] = useState(null);
   const dispatch = useDispatch();
-  const addNewItem = () => {
-    dispatch(setList(newTask));
+  const handleKeyDown = event => {
+    if (event.key === 'Enter') {
+      dispatch(setList(newTask));
+      setNewTask('');
+    }
   };
   return (
     <div className="header-container">
       <div className="input-div">
         <input
+          onKeyDown={handleKeyDown}
+          name="newTask"
+          value={newTask}
           onChange={e => setNewTask(e.target.value)}
           placeholder="What needs to be done?"></input>
-      </div>
-      <div className="icon-div">
-        <button className="icon-button" onClick={addNewItem}>
-          <FaIcons.FaPlusCircle className="add-icon" />
-        </button>
       </div>
     </div>
   );
