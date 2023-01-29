cad1b7a8007fe7de7f82f406e45c084e6fd143b4
Atakanz <atakanzer@gmail.com>
2023-01-29 05:05:31 +0300

    List unit components moved to the container.

--- to-do-list/src/components/container/container.js
+++ to-do-list/src/components/container/container.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './container.css';
 import Footer from '../footer/footer';
-import ListSection from '../listSection/listSection';
 import Header from '../header/header';
+import {useSelector} from 'react-redux';
+import ListUnits from '../listUnits/listUnits';
 
 const Container = () => {
+  const [currentList, setCurrentList] = useState([]);
+  const list = useSelector(state => state.list.list);
+  const doneTasks = useSelector(state => state.list.doneList);
+  const activeOnes = list.filter(item => {
+    return doneTasks.indexOf(item) === -1;
+  });
+  const setActiveOnes = () => {
+    setCurrentList(activeOnes);
+  };
+  const setAll = () => {
+    setCurrentList(list);
+  };
+  const setDone = () => {
+    setCurrentList(doneTasks);
+  };
+  useEffect(() => {
+    setCurrentList(list);
+  }, [list]);
+  const todoItems = currentList.map((todo, index) => (
+    <li key={index}>
+      <ListUnits text={todo} />
+    </li>
+  ));
   return (
     <>
       <section className="container">
         <Header />
-        <ListSection />
-        <Footer />
+        {todoItems}
+        <Footer
+          activeNumber={activeOnes.length}
+          activesTask={setActiveOnes}
+          allTask={setAll}
+          doneTask={setDone}
+        />
       </section>
     </>
   );
