87d5dcf7ea3c9be75a9f70f6053d32f7d04951df
Atakanz <atakanzer@gmail.com>
2023-01-29 05:09:28 +0300

    Clear completed slice function added.

--- to-do-list/src/components/footer/footer.js
+++ to-do-list/src/components/footer/footer.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import './footer.css';
 import Buttons from '../buttons/buttons';
+import {useDispatch} from 'react-redux';
+import {filterCompleted} from '../../management/features/listSlice';
 
-const Footer = () => {
+const Footer = props => {
+  const dispatch = useDispatch();
+  const clearCompleted = () => {
+    dispatch(filterCompleted([]));
+  };
+
   return (
     <>
       <div className="footer-container">
-        <text>3 items left</text>
+        <text>{props.activeNumber} items left</text>
         <div className="middle-section">
-          <Buttons buttonTitle="All"></Buttons>
-          <Buttons buttonTitle="Active"></Buttons>
-          <Buttons buttonTitle="Completed"></Buttons>
+          <Buttons buttonTitle="All" buttonTask={props.allTask}></Buttons>
+          <Buttons
+            buttonTitle="Active"
+            buttonTask={props.activesTask}></Buttons>
+          <Buttons
+            buttonTitle="Completed"
+            buttonTask={props.doneTask}></Buttons>
         </div>
-        <Buttons buttonTitle="Clear completed"></Buttons>
+        <Buttons
+          buttonTitle="Clear completed"
+          buttonTask={clearCompleted}></Buttons>
       </div>
     </>
   );
