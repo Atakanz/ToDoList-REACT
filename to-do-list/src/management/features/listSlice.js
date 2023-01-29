cc3a72b7dbce4b6407dc1eeae550b847ba10b9d9
Atakanz <atakanzer@gmail.com>
2023-01-29 05:06:19 +0300

    Edit item, add done&undo add done functions added.

--- to-do-list/src/management/features/listSlice.js
+++ to-do-list/src/management/features/listSlice.js
@@ -2,6 +2,7 @@ import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
   list: [],
+  doneList: [],
 };
 
 export const listSlice = createSlice({
@@ -11,9 +12,29 @@ export const listSlice = createSlice({
     setList: (state, action) => {
       state.list = [...state.list, action.payload];
     },
+    setDoneList: (state, action) => {
+      state.doneList = [...state.doneList, action.payload];
+    },
+    undoDoneList: (state, action) => {
+      state.doneList = state.doneList.filter(item => item !== action.payload);
+    },
+    editItem: (state, action) => {
+      state.list.forEach((element, index) => {
+        if (element === action.payload[0]) {
+          state.list[index] = action.payload[1];
+        }
+      });
+    },
+    filterCompleted: (state, action) => {
+      state.list = state.list.filter(
+        item => state.doneList.indexOf(item) === -1,
+      );
+      state.doneList = action.payload;
+    },
   },
 });
 
-export const {setList} = listSlice.actions;
+export const {setList, setDoneList, undoDoneList, filterCompleted, editItem} =
+  listSlice.actions;
 
 export default listSlice.reducer;
